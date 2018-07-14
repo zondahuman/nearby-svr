1b82849e2729fbf45b2162065852fa5203c6dac7
zondahuman <zondahuman@gmail.com>
2018-07-14 23:12:01 +0800

    add

--- nearby-common/src/main/resources/nearby.sh
+++ nearby-common/src/main/resources/nearby.sh
@@ -10,6 +10,8 @@ kill -9 $pidlist
 echo "KILL $pidlist:"
 echo "service stop success"
 echo "start tomcat"
-rm -rf /home/work/local/tomcat-house/webapps/house
-nohup /home/work/local/tomcat-house/bin/startup.sh &
-tail -f /home/work/local/tomcat-house/logs/catalina.out
+#rm -rf /root/soft/nearby-svr/webapps/ROOT
+nohup /root/soft/nearby-svr/bin/startup.sh &
+tail -f /root/soft/nearby-svr/logs/catalina.out
+
+
