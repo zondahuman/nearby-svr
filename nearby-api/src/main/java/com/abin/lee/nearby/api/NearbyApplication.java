240c7cad3746d245dc42b9081bd4632c337e1bf3
zondahuman <zondahuman@gmail.com>
2018-07-14 18:37:44 +0800

    add

--- nearby-api/src/main/java/com/abin/lee/nearby/api/NearbyApplication.java
+++ nearby-api/src/main/java/com/abin/lee/nearby/api/NearbyApplication.java
@@ -3,15 +3,17 @@ package com.abin.lee.nearby.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 /**
  * Created by abin on 2018/7/4.
+ * https://blog.csdn.net/soldier_123/article/details/78917227
  */
 @Slf4j
 @SpringBootApplication
-public class NearbyApplication {
-
+public class NearbyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(NearbyApplication.class, args);
@@ -21,7 +23,22 @@ public class NearbyApplication {
         }
 
         SpringApplication.run(NearbyApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
     }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(NearbyApplication.class);
+    }
+
+
+
 
 
 }
