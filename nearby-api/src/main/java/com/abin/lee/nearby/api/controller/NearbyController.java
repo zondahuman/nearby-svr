3b02ede2258ed436cce4d60479623ea4163af0e5
zondahuman <zondahuman@gmail.com>
2018-07-22 19:44:35 +0800

    add

--- nearby-api/src/main/java/com/abin/lee/nearby/api/controller/NearbyController.java
+++ nearby-api/src/main/java/com/abin/lee/nearby/api/controller/NearbyController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
@@ -25,6 +28,22 @@ public class NearbyController {
         String localParam = env.getProperty("local.param");
         log.info("---------param= " + param + "  , localParam= " + localParam);
         return localParam;
+    }
+
+
+    @RequestMapping(value = "/create", method = {RequestMethod.GET, RequestMethod.POST})
+    public String create(Integer param) {
+        List<String> caches = new ArrayList<String>();
+        for (int i = 0; i < 10000000; i++) {
+            StringBuffer buffer = new StringBuffer();
+            for (int j = 0; j <100 ; j++) {
+                buffer.append("leeooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"+j);
+            }
+
+            caches.add(buffer.toString() + i);
+//            caches.add(new byte[1024 * 1024 * 3]);
+        }
+        return "SUCCESS";
     }
 
 
--- nearby-api/src/test/java/com/abin/lee/nearby/api/test/NearbyMemoryTest.java
+++ nearby-api/src/test/java/com/abin/lee/nearby/api/test/NearbyMemoryTest.java
@@ -11,33 +11,16 @@ import java.util.Map;
  * Created by abin on 2018/7/12.
  *  http://localhost:9477/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz
  */
-public class NearbyEnvironmentTest {
+public class NearbyMemoryTest {
     private static final String httpUrl = "http://localhost:9333/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz";
-    private static final String httpCreateUrl = "http://localhost:8444/nearby/add";
+    private static final String httpCreateUrl = "http://localhost:8444/nearby/create";
 //    private static final String httpCreateUrl = "http://localhost:9000/nearby/add";
 //    private static final String httpCreateUrl = "http://localhost:9333/nearby/add";
 
-    public static void main(String[] args) throws IOException {
 
-
-
-
-    }
 
     @Test
-    public void testCloudZuulClient1() throws IOException {
-        String result = OkHttpClientUtil.httpPost(httpUrl, null);
-        System.out.println("result="+result);
-    }
-
-    @Test
-    public void testCloudZuulClient2() throws IOException {
-        String result = OkHttpClientUtil.httpGet(httpUrl);
-        System.out.println("result="+result);
-    }
-
-    @Test
-    public void testNearbyEnvironment() throws IOException {
+    public void testNearbyMemory() throws IOException {
         Map<String, String> params = Maps.newHashMap();
         params.put("param1", "123");
 
