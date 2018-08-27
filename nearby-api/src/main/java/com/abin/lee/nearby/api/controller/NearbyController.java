975935d823c0e5091c90f97a464e7a35b745f87e
zondahuman <zondahuman@gmail.com>
2018-08-27 14:03:48 +0800

    add

--- nearby-api/src/main/java/com/abin/lee/nearby/api/controller/NearbyController.java
+++ nearby-api/src/main/java/com/abin/lee/nearby/api/controller/NearbyController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,9 +25,20 @@ public class NearbyController {
 
 
     @RequestMapping(value = "/add", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String add(Integer param) {
         String localParam = env.getProperty("local.param");
         log.info("---------param= " + param + "  , localParam= " + localParam);
+        System.out.println("---------param= " + param + "  , localParam= " + localParam);
+        return localParam;
+    }
+
+    @RequestMapping(value = "/add1", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String add1(Integer param) {
+        String localParam = env.getProperty("all.param");
+        log.info("---------param= " + param + "  , localParam= " + localParam);
+        System.out.println("---------param= " + param + "  , localParam= " + localParam);
         return localParam;
     }
 
--- nearby-api/src/test/java/com/abin/lee/nearby/api/test/NearbyMemoryTest.java
+++ nearby-api/src/test/java/com/abin/lee/nearby/api/test/NearbyMemoryTest.java
@@ -13,20 +13,32 @@ import java.util.Map;
  */
 public class NearbyMemoryTest {
     private static final String httpUrl = "http://localhost:9333/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz";
-    private static final String httpCreateUrl = "http://localhost:8444/nearby/create";
+//    private static final String httpCreateUrl = "http://localhost:8333/nearby/create";
+//    private static final String httpCreateUrl = "http://localhost:8444/nearby/create";
 //    private static final String httpCreateUrl = "http://localhost:9000/nearby/add";
-//    private static final String httpCreateUrl = "http://localhost:9333/nearby/add";
+    private static final String httpAdd = "http://localhost:8333/nearby/add";
+    private static final String httpAdd1 = "http://localhost:8333/nearby/add1";
 
 
 
     @Test
     public void testNearbyMemory() throws IOException {
         Map<String, String> params = Maps.newHashMap();
-        params.put("param1", "123");
+        params.put("param", "123");
 
-        String result = OkHttpClientUtil.httpPost(httpCreateUrl, params);
+        String result = OkHttpClientUtil.httpPost(httpAdd, params);
         System.out.println("result="+result);
     }
+
+    @Test
+    public void testNearbyMemory1() throws IOException {
+        Map<String, String> params = Maps.newHashMap();
+        params.put("param", "456");
+
+        String result = OkHttpClientUtil.httpPost(httpAdd1, params);
+        System.out.println("result="+result);
+    }
+
 
 
 
